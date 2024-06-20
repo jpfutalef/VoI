c79e17ee1782e921ad8e608a775b51a5d8c1523e
Juan-Pablo Futalef <jpfutalef@gmail.com>
2024-06-20 12:36:04 +0200

    Added tools for simplified lack of fit calculations

--- losses_calculations/lack_of_fit_risk2.py
+++ losses_calculations/lack_of_fit_risk2.py
@@ -10,16 +10,27 @@ from losses_calculations.setup_vars import *
 from greyboxmodels.metrics import lack_of_fit as metric
 
 # %% Create a folder to store the values
-target_dir = setup_dir(metric)
+target_dir = setup_dir(metric, subdirectory="risk_metric_2")
 reference_dir = Path(os.environ["VOI_ROOT"]) / "data/simulation_references/iptlc/MonteCarlo/2024-05-09_15-16-30/"
+
+
+#%% open plant
+from greyboxmodels.cpsmodels.Plant import Plant
+from greyboxmodels.cpsmodels.cyberphysical.IPTLC.IPTLC import IPandTLC
+
+plant_dir = Path(os.environ["MODELS_ROOT"]) / "data/wbm-models/iptlc_ieee14-deterministic_tlcn7_wbm.pkl"
+plant: IPandTLC = Plant.load(plant_dir)
 
 
 # %% The state filter
 def state_filter(i_x):
-    # Returns true if the state is to be dropped (filters out the state)
-    if i_x > 79:
-        # In the power grid, the first 80 states are the continuous states.
-        # The rest are the discrete states and the variables from other devices.
+    import numpy as np
+    # Only power demands in the state
+    pg_idx = plant.power_grid.state_idx.Pg
+    qg_idx = plant.power_grid.state_idx.Qg
+    target_idx = np.concatenate([pg_idx, qg_idx])
+
+    if i_x not in target_idx:
         return True
     return False
 
@@ -31,56 +42,3 @@ table, info = metric.folders_comparison(folders,
                                         names,
                                         state_filter=state_filter,
                                         )
-
-# %% Some figures
-import matplotlib.pyplot as plt
-import dill as pickle
-import numpy as np
-import matplotlib as mpl
-
-mpl.use('TkAgg')
-
-info_loc = "data/voi_losses/lack_of_fit/2024-05-09_15-16-30/lack_of_fit_info.pkl"
-
-with open(info_loc, "rb") as f:
-    info = pickle.load(f)
-
-# %% Get target values
-model = 0
-l = list(info.keys())
-d = info[l[model]]["detailed_info"]
-n_states = len(d)
-
-# %% Create the figure
-plt.close("all")
-
-# Compute a nice number of rows and columns
-n_cols = int(np.ceil(np.sqrt(n_states)))
-n_rows = int(np.ceil(n_states / n_cols))
-
-fig, axs = plt.subplots(n_rows, n_cols, figsize=(16, 8), sharex=True)
-axs = axs.flatten()
-
-for i_X, dinfo in d.items():
-    t = [x[0] for x in dinfo]
-    lof = [x[1] for x in dinfo]
-
-    ax = axs[i_X]
-
-    ax.plot(t, lof)
-    ax.set_title(f"State {i_X}")
-    ax.set_xlim([0, t[-1]])
-    ax.set_ylim([0, 1])
-
-    if i_X % n_cols == 0:
-        ax.set_ylabel("LoF")
-
-    if i_X >= n_states - n_cols:
-        ax.set_xlabel("Time (s)")
-
-# Remove the unused axes
-for i_X in range(n_states, len(axs)):
-    fig.delaxes(axs[i_X])
-
-plt.tight_layout()
-plt.show()
--- losses_calculations/setup_vars.py
+++ losses_calculations/setup_vars.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 # %% Specify paths
 wbm = Path("/mnt/d/projects/CPS-SenarioGeneration/data/iptlc/MonteCarlo/2024-05-09_15-16-30").resolve()
@@ -16,16 +17,31 @@ names = ["WBM" if i == 0 else f"GBM{i}" for i, _ in enumerate(folders)]
 
 
 # %% Function to setup the path
-def setup_dir(module):
+def setup_dir(module, subdirectory=None):
     import os
     root = Path(os.environ["VOI_ROOT"])
     mod_name = module.__name__.split(".")[-1]
-    target_dir = root / "data" / "voi_losses" / mod_name / wbm.name
+
+    if subdirectory:
+        target_dir = root / "data" / "voi_losses" / mod_name / wbm.name / subdirectory
+
+    else:
+        target_dir = root / "data" / "voi_losses" / mod_name / wbm.name
 
     if target_dir.exists():
-        print(f"WARNING: The target directory already exists. It will be overwritten.\n   {target_dir}")
+        while True:
+            r = input(
+                f"WARNING: The target directory already exists. It will be overwritten.\n   {target_dir} \nContinue? (y/n)")
+
+            if r.lower() == "n":
+                print("Exiting...")
+                sys.exit()
+
+            elif r.lower() == "y":
+                break
+
+            else:
+                print("Invalid option. Please type 'y' or 'n'.")
 
     os.makedirs(target_dir, exist_ok=True)
     return target_dir
-
-
